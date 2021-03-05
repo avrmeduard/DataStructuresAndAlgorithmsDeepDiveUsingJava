47f55aeeb8aa4290cf49c0f6c8f84dbc001acac8
avrm <avrmeduard@gmail.com>
2021-03-05 10:23:23 +0200

    + InsertionSort --> O(n2) time complexity; stable sort - second inplementation

--- src/com/avrm/sortalgorithms/InsertionSort.java
+++ src/com/avrm/sortalgorithms/InsertionSort.java
@@ -7,6 +7,25 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {20, 35, -15, 7, 55, 1, -22, 2};
 
+        insertionSort2(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertionSort2(int[] arr) {
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
+
+            int newElement = arr[firstUnsortedIndex];
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && arr[i-1] > newElement ; i--) {
+                arr[i] = arr[i-1];
+            }
+            arr[i] = newElement;
+        }
+    }
+    
+    public static void insertionSort(int[] arr) {
         for (int j = 0; j < arr.length; j++) {
             int key = arr[j];
             int i = j-1;
@@ -17,12 +36,7 @@ public class InsertionSort {
             }
             arr[i+1] = key;
         }
-
-
-        System.out.println(Arrays.toString(arr));
     }
-
-
 
 
 }
