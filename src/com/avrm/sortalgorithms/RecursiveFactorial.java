76390787c3c60d202738ba225184fe5ce9f38c3f
avrm <avrmeduard@gmail.com>
2021-03-05 11:03:03 +0200

    +

--- src/com/avrm/sortalgorithms/RecursiveFactorial.java
+++ src/com/avrm/sortalgorithms/RecursiveFactorial.java
@@ -1,4 +1,21 @@
 package com.avrm.sortalgorithms;
 
+import java.util.Arrays;
+
 public class RecursiveFactorial {
+
+    public static void main(String[] args) {
+
+        int[] arr = {20, 35, -15, 7, 55, 1, -22, 2};
+        recursiveFactorial(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void recursiveFactorial(int[] arr) {
+
+
+    }
+
+
 }
